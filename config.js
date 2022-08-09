34c801a10a02d84cbc038bd0fa35271db0ac4f33
Senpai <90470942+Senpai723@users.noreply.github.com>
2022-08-09 12:13:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,11 +18,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','62895336722950','6287721317870','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
-global.author = 'WhatsApp Bot'
-global.sessionName = 'hisoka'
+global.owner = ['6282314858723']
+global.premium = ['6282314858723']
+global.packname = 'SenBOT'
+global.author = 'Dibuat Oleh SenBOT'
+global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
